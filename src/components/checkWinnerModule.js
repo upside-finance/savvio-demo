c5a111dbd2aeb55f7d82376d945a67e85c5e6c1d
npanium <npanium@gmail.com>
2023-03-13 12:58:57 +1100

    added winner result ui to check winner module

--- src/components/checkWinnerModule.js
+++ src/components/checkWinnerModule.js
@@ -14,6 +14,7 @@ export default function CheckWinnerModule() {
   const dispatch = useDispatch();
   const active = useSelector((state) => state.ui.showCheckWinnerModule);
   const [visibility, setVisibility] = useState(false);
+  const [winner, setWinner] = useState(false);
 
   //animation of the spinner
   const [spinnerSpeed, setSpinnerSpeed] = useState(0);
@@ -22,22 +23,28 @@ export default function CheckWinnerModule() {
   // Set visibility after a delay for the slide up animation to be visible. for some reason only works if the "check draw" button is pressed once
   setTimeout(() => setVisibility(true), 100);
 
+  //modal exit transition seems to work only sometimes
   var modalClass = visibility
-    ? "z-50 fixed flex-col  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 m-auto rounded-2xl p-2 ease-in-out duration-300 translate-y-0 opacity-500 w-[30rem] max-w-full h-[39rem] max-h-full overflow-x-clip overflow-y-scroll lg:overflow-clip border border-green-aqua bg-white shadow-lemon"
-    : "z-50 fixed flex-col top-1/2 left-1/2 -translate-x-1/2 m-auto rounded-2xl p-2 ease-in-out duration-300 translate-y-full opacity-0 w-[30rem] max-w-full h-[39rem] max-h-full overflow-x-clip overflow-y-scroll lg:overflow-clip border border-green-aqua bg-white shadow-lemon";
+    ? "z-50 fixed flex-col justify- inset-y-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 m-auto rounded-2xl p-2 ease-in-out duration-300 translate-y-0 opacity-500 w-[30rem] max-w-full h-[39rem] max-h-full overflow-x-clip overflow-y-scroll lg:overflow-clip border border-green-aqua shadow-lemon "
+    : "z-50 fixed flex-col inset-y-1/2 left-1/2 -translate-x-1/2 m-auto rounded-2xl p-2 ease-in-out duration-300 translate-y-1/2 opacity-0 w-[30rem] max-w-full h-[39rem] max-h-full overflow-x-clip overflow-y-scroll lg:overflow-clip border border-green-aqua shadow-lemon ";
 
   return ReactDom.createPortal(
     <>
       <div
         onClick={() => {
           setVisibility(false);
-          setTimeout(() => dispatch(setShowCheckWinnerModule(false)), 120);
+          setTimeout(() => dispatch(setShowCheckWinnerModule(false)), 100);
         }}
         className={`z-50 fixed inset-0 bg-black/70 backdrop-blur ease-in-out
         ${active ? "opacity-100" : "hidden"}`}
       />
 
-      <div className={modalClass}>
+      <div
+        className={
+          modalClass +
+          (winner == null ? "bg-white" : winner ? "gradient-aqua" : "bg-white")
+        }
+      >
         <IconContext.Provider
           value={{
             className: "ml-auto mr-5 my-5 text-gray text-2xl ",
@@ -53,32 +60,75 @@ export default function CheckWinnerModule() {
             <IoClose />
           </div>
         </IconContext.Provider>
-        <h3 className="text-green-aqua uppercase text-4xl text-center">
-          Check if <br /> you’re a Winner
-        </h3>
-        <div className="m-5">
-          <Lottie
-            loop
-            play
-            speed={spinnerSpeed}
-            animationData={spinner}
-            className={`h-60 w-auto overfloy-x-clip m-auto py-4 md:py-0 stroke-[0.5px] `}
-          />
+        {winner == null ? (
+          <div className="h-[90%] flex flex-col">
+            <h3 className="text-green-aqua uppercase text-4xl text-center">
+              Check if <br /> you’re a Winner
+            </h3>
+            <div className="m-5">
+              <Lottie
+                loop
+                play
+                speed={spinnerSpeed}
+                animationData={spinner}
+                className={`h-60 w-auto overfloy-x-clip m-auto py-4 md:py-0 stroke-[0.5px] `}
+              />
 
-          <button
-            onClick={() =>
-              setSpinnerSpeed(spinnerSpeed == 0 ? animation * 100 - 100 : 0)
-            }
-            className="w-full mx-auto mt-10 px-5 py-2  button-gradient button-gradient-aqua"
-          >
-            Check now
-          </button>
+              <button
+                onClick={() =>
+                  setSpinnerSpeed(spinnerSpeed == 0 ? animation * 100 - 100 : 0)
+                }
+                className="w-full mx-auto mt-10 px-5 py-2 button-gradient button-gradient-aqua"
+              >
+                Check now
+              </button>
 
-          <p className="text-sm text-slate-700 my-2">
-            Bid will be held in escrow until there is a higher bid or until the
-            auction ends
-          </p>
-        </div>
+              <p className="text-sm text-slate-700 my-2">
+                Bid will be held in escrow until there is a higher bid or until
+                the auction ends
+              </p>
+            </div>
+          </div>
+        ) : winner ? (
+          <div className="h-[90%] flex flex-col">
+            <h3 className="my-auto text-white text-5xl text-center leading-[5rem]">
+              Congratulations <br /> <span className="text-6xl">You’re a </span>
+              <br /> <span className="text-7xl uppercase">Winner!</span>
+            </h3>
+            <div className="my-10 mx-5">
+              <button
+                onClick={() =>
+                  setSpinnerSpeed(spinnerSpeed == 0 ? animation * 100 - 100 : 0)
+                }
+                className="w-full mx-auto mt-10 px-5 py-2 bg-white button-aqua"
+              >
+                Claim now
+              </button>
+
+              <p className="text-sm text-slate-700 my-2">
+                Bid will be held in escrow until there is a higher bid or until
+                the auction ends
+              </p>
+            </div>
+          </div>
+        ) : (
+          <div className="h-[90%] flex flex-col">
+            <h3 className="my-auto text-green-aqua text-8xl text-center leading-[5rem]">
+              Sorry <br /> <span className="text-6xl">Better luck </span>
+              <br /> <span className="text-6xl ">next time</span>
+            </h3>
+            <div className="my-10 mx-5">
+              <button className="w-full mx-auto mt-10 px-5 py-2 bg-white shadow-md shadow-gray-300 rounded text-gray-300 cursor-default">
+                Claim now
+              </button>
+
+              <p className="text-sm text-gray-300 my-2">
+                Bid will be held in escrow until there is a higher bid or until
+                the auction ends
+              </p>
+            </div>
+          </div>
+        )}
       </div>
     </>,
 
