ca79e1a98e84d984d9cc8286d85ac776417d5f3e
Tejas Kothari <tejas23682320@gmail.com>
2023-03-15 01:10:26 +0800

    Sucessful stake and error screens in modal

--- src/components/ticketsModule.js
+++ src/components/ticketsModule.js
@@ -4,6 +4,7 @@ import ReactDom from "react-dom";
 import { IoClose } from "react-icons/io5";
 import { IconContext } from "react-icons";
 import NumericInput from "react-numeric-input";
+import { GridLoader } from "react-spinners";
 
 import nftimage from "../assets/thumb-nft.png";
 import { useDispatch, useSelector } from "react-redux";
@@ -29,6 +30,11 @@ export default function TicketsModule({ gameID }) {
   const [isLoading, setIsLoading] = useState(false);
   const [errorScreen, setErrorScreen] = useState(false);
   const [claimSuccess, setClaimSuccess] = useState(false);
+
+  const onClose = () => {
+    setVisibility(false);
+    setTimeout(() => dispatch(setShowTicketsModule(false)), 100);
+  };
 
   const checkIfValidInput = () => {
     if (stakeAmount != null) {
@@ -113,74 +119,100 @@ export default function TicketsModule({ gameID }) {
   return ReactDom.createPortal(
     <>
       <div
-        onClick={() => {
-          setVisibility(false);
-          setTimeout(() => dispatch(setShowTicketsModule(false)), 100);
-        }}
+        onClick={onClose}
         className={`z-50 fixed inset-0 bg-black/70 backdrop-blur ease-in-out
         ${active ? "opacity-100" : "hidden"}`}
       />
       <div className={modalClass}>
+        {isLoading ? (
+          <div className="w-full h-full inset-1/2 -translate-x-1/2 -translate-y-1/2 absolute bg-slate-900/70 flex justify-center items-center">
+            <GridLoader color={"#FBCFE8"} />
+          </div>
+        ) : null}
         <IconContext.Provider
           value={{
             className: "ml-auto mr-5 my-5 text-gray text-2xl",
           }}
         >
-          <div
-            onClick={() => {
-              setVisibility(false);
-              setTimeout(() => dispatch(setShowTicketsModule(false)), 100);
-            }}
-            className="cursor-pointer"
-          >
+          <div onClick={onClose} className="cursor-pointer">
             <IoClose />
           </div>
         </IconContext.Provider>
 
-        <div className="h-[90%] flex flex-col">
-          <h3 className="text-green-aqua uppercase text-3xl text-center">
-            Claim your tickets now
-          </h3>
-          <div className="m-5">
-            <img
-              src={nftimage}
-              alt="NFT artwork"
-              className="rounded-xl w-60 h-60 m-auto shadow-small"
-            />
-            <p className="text-green-aqua mt-5 mb-2">
-              {toSU(userCoinBal, decimals)} Available APT (1 APT = 1 Ticket)
-            </p>
-            <input
-              value={stakeAmountStr}
-              onChange={(e) => {
-                let numStr = e.target.value;
-                const numDP = numOfDP(numStr);
-                numStr =
-                  numDP > decimals
-                    ? numStr.slice(0, -(numDP - decimals))
-                    : numStr;
-                numStr = numStr.replace("-", "");
-                setStakeAmountStr(numStr);
-              }}
-              type="number"
-              step=".01"
-              min={0}
-              placeholder="35.40"
-              className="z-20 w-full h-14 p-2 relative text-xl text-gray-dark placeholder:text-gray-light rounded-lg border-2 border-green-aqua shadow-md"
-            />
-            <div className="text-red-500 text-sm mt-1">{inputErrorMsg}</div>
-            <button
-              onClick={onStakeClick}
-              className="z-20 w-full ml-0 mt-5 button-aqua gradient-border bg-white hover:border-transparent relative "
+        {errorScreen ? (
+          <div className="flex flex-col justify-center items-center h-3/4 ">
+            <h3 className="text-green-aqua uppercase text-3xl text-center">
+              Something went wrong!
+            </h3>
+            <h3 className="text-green-aqua text-xl text-center">
+              Please try again
+            </h3>
+            <div
+              onClick={onClose}
+              className="z-20 w-max-xs md:w-2/3 ml-0 mt-5 button-aqua gradient-border bg-white hover:border-transparent relative"
             >
-              Claim your tickets
-            </button>
-            <p className="text-sm text-gray my-2">
-              Bid will be held in escrow until there is a higher bid or until
-              the auction ends
-            </p>
+              Close
+            </div>
           </div>
-        </div>
+        ) : claimSuccess ? (
+          <div className="flex flex-col justify-center items-center h-3/4 ">
+            <h3 className="text-green-aqua uppercase text-4xl text-center">
+              Sucessfully <br />
+              Staked {stakeAmountStr} APT
+            </h3>
+            <div
+              onClick={onClose}
+              className="z-20 w-max-xs md:w-2/3 ml-0 mt-5 button-aqua gradient-border bg-white hover:border-transparent relative"
+            >
+              Close
+            </div>
+          </div>
+        ) : (
+          <div className="h-[90%] flex flex-col">
+            <h3 className="text-green-aqua uppercase text-3xl text-center">
+              Claim your tickets now
+            </h3>
+            <div className="m-5">
+              <img
+                src={nftimage}
+                alt="NFT artwork"
+                className="rounded-xl w-60 h-60 m-auto shadow-small"
+              />
+              <p className="text-green-aqua mt-5 mb-2">
+                {toSU(userCoinBal, decimals)} Available APT (1 APT = 1 Ticket)
+              </p>
+              <input
+                value={stakeAmountStr}
+                onChange={(e) => {
+                  let numStr = e.target.value;
+                  const numDP = numOfDP(numStr);
+                  numStr =
+                    numDP > decimals
+                      ? numStr.slice(0, -(numDP - decimals))
+                      : numStr;
+                  numStr = numStr.replace("-", "");
+                  setStakeAmountStr(numStr);
+                }}
+                type="number"
+                step=".01"
+                min={0}
+                placeholder="35.40"
+                className="z-20 w-full h-14 p-2 relative text-xl text-gray-dark placeholder:text-gray-light rounded-lg border-2 border-green-aqua shadow-md"
+              />
+              <div className="text-red-500 text-sm mt-1">{inputErrorMsg}</div>
+              <button
+                onClick={onStakeClick}
+                className="z-20 w-full ml-0 mt-5 button-aqua gradient-border bg-white hover:border-transparent relative "
+              >
+                Claim your tickets
+              </button>
+              <p className="text-sm text-gray my-2">
+                Bid will be held in escrow until there is a higher bid or until
+                the auction ends
+              </p>
+            </div>
+          </div>
+        )}
       </div>
     </>,
 
