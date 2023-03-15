9efc9de41264d92f7371b055b9015325c1f10c3e
Tejas Kothari <tejas23682320@gmail.com>
2023-03-15 22:45:28 +0800

    update module addr

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const NODE_URL = "https://fullnode.devnet.aptoslabs.com/v1";
 export const NFT_PRIZE_GAME_MODULE_ADDR =
-  "0x3676689f84201e34386cc4bfc08aa0c20293a7556d04b0ec1620f3f06181d96e";
+  "0xf39ed7b9d65481e909f0e43e9af4307a2b3af8ab8579c4db5b8434eea976127c";
 export const DATA_FETCHING_FREQ_MS = 30000;
